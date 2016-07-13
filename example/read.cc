b998c3f81de746092418b2e59312ce474d4c4d94
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-07-13 12:36:23 +0200

    Add graph example to read.

--- example/read.cc
+++ example/read.cc
@@ -1,4 +1,4 @@
-#include "datamodel/ParticleCollection.h"
+#include "datamodel/MCParticleCollection.h"
 #include "datamodel/EventInfoCollection.h"
 #include "datamodel/JetCollection.h"
 #include "datamodel/JetParticleAssociationCollection.h"
@@ -7,6 +7,7 @@
 #include "utilities/JetUtils.h"
 #include "utilities/VectorUtils.h"
 #include "utilities/ParticleUtils.h"
+#include "utilities/GraphBuilder.h"
 
 // ROOT
 #include "TBranch.h"
@@ -23,8 +24,22 @@
 #include "podio/EventStore.h"
 #include "podio/ROOTReader.h"
 
-void processEvent(podio::EventStore& store, bool verbose,
-                  podio::ROOTReader& reader) {
+std::ostream& operator<<(std::ostream& out, const fcc::MCParticle& ptc) {
+  if(not out) return out;
+  const fcc::BareParticle& pcore = ptc.Core();
+  TLorentzVector p4 = utils::lvFromPOD(pcore.P4);
+  out<< "particle ID " << pcore.Type
+     << " e " << p4.E()
+     << " pt " << p4.Pt()
+     << " eta " << p4.Eta()
+     << " phi " << p4.Phi();
+  return out;
+}
+
+void processEvent(podio::EventStore& store,
+                  bool verbose,
+                  podio::ROOTReader& reader,
+                  fcc::GraphBuilder& graph) {
 
   // read event information
   const fcc::EventInfoCollection* evinfocoll(nullptr);
@@ -37,10 +52,13 @@ void processEvent(podio::EventStore& store, bool verbose,
   }
 
   // read particles
-  const fcc::ParticleCollection* ptcs(nullptr);
+  const fcc::MCParticleCollection* ptcs(nullptr);
   bool particles_available = store.get("GenParticle", ptcs);
   if (particles_available){
-    std::vector<fcc::Particle> muons;
+    graph.build(*ptcs);
+    DAG::BFSVisitor<fcc::IdNode> visitor;
+    std::vector<fcc::MCParticle> muons;
+    std::vector<fcc::MCParticle> higgses;
     // there is probably a smarter way to get a vector from collection?
 
     if(verbose)
@@ -50,6 +68,28 @@ void processEvent(podio::EventStore& store, bool verbose,
         std::cout<<"\t"<<ptc<<std::endl;
       if( ptc.Core().Type == 4 ) {
         muons.push_back(ptc);
+      } else if (ptc.Core().Type == 25) {
+        higgses.push_back(ptc);
+      }
+    }
+    for (const auto& higgs : higgses) {
+      const fcc::IdNode& higgsNode = graph.getNode(higgs);
+      if (verbose) {
+        std::cout << higgs << std::endl;
+        std::cout << "decayproducts;" << higgsNode.children().size() << std::endl;
+        std::cout << "     decay products: ";
+      }
+      for (auto n : visitor.traverseChildren(higgsNode)) {
+        // not needed because we are working with only one collection:
+        // const fcc::MCParticleCollection* ptcs(nullptr);
+        // bool particles_available = store.get(n->value.collectionID, ptcs);
+        const auto& topDaughter = ptcs->at(n->value().index);
+        if (verbose) {
+          std::cout << topDaughter << ", ";
+        }
+      }
+      if (verbose) {
+        std::cout << std::endl;
       }
     }
   }
@@ -58,6 +98,7 @@ void processEvent(podio::EventStore& store, bool verbose,
 int main(int argc, char** argv){
   auto reader = podio::ROOTReader();
   auto store = podio::EventStore();
+  auto graph = fcc::GraphBuilder();
   if( argc != 2) {
     std::cerr<<"Usage: pythiafcc-read filename"<<std::endl;
     return 1;
@@ -83,7 +124,8 @@ int main(int argc, char** argv){
     if(i>10) {
       verbose = false;
     }
-    processEvent(store, verbose, reader);
+    processEvent(store, verbose, reader, graph);
+    graph.clear();
     store.clear();
     reader.endOfEvent();
   }
